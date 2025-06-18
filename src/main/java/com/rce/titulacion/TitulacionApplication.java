ca1003b5a95510c80fd04c66401c508382baba4f
Kevin Holguin <kevinliga525@gmail.com>
2025-06-18 18:57:46 -0500

    init: proyecto Spring Boot base de titulacion

---
+++ src/main/java/com/rce/titulacion/TitulacionApplication.java
@@ -0,0 +1,13 @@
+package com.rce.titulacion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitulacionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitulacionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rce/titulacion/TitulacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rce.titulacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitulacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
